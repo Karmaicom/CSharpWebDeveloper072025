9e42450d0ee5d8bdb51efb779d0508f5f28580ac
karmaicom <karmaicom@gmail.com>
2025-07-15 21:35:38 -0300

    config: Criação da classe para reuso da connection string.

---
+++ Aula02_ControleDeFuncionarios/Aula02_ControleDeFuncionarios/Configuracoes/ConfiguracaoDB.cs
@@ -0,0 +1,15 @@
+﻿namespace Aula02_ControleDeFuncionarios.Configuracoes
+{
+    public class ConfiguracaoDB
+    {
+
+        public string ConnectioString
+        {
+            get
+            {
+                return "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBFuncionarios;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            }
+        }
+
+    }
+}
