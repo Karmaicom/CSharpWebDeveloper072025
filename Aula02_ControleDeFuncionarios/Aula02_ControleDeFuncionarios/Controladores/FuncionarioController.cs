293889fd7d064e3aa64cdaf1fd041c3c48c39687
karmaicom <karmaicom@gmail.com>
2025-07-15 21:33:29 -0300

    feat: Ciração do Controller, Repositorio e Response do Funcionario

---
+++ Aula02_ControleDeFuncionarios/Aula02_ControleDeFuncionarios/Controladores/FuncionarioController.cs
@@ -0,0 +1,41 @@
+﻿using Aula02_ControleDeFuncionarios.Entidades;
+using Aula02_ControleDeFuncionarios.Repositorios;
+using Aula02_ControleDeFuncionarios.Repositorios.Responses;
+
+namespace Aula02_ControleDeFuncionarios.Controladores
+{
+    public class FuncionarioController
+    {
+        private readonly FuncionarioRepositorio _repositorio;
+
+        public FuncionarioController()
+        {
+            _repositorio = new FuncionarioRepositorio();
+        }
+
+        public InserirFuncionarioResponse CadastrarFuncionario(Funcionario funcionario)
+        {
+            try
+            {
+                _repositorio.InserirFuncionario(funcionario);
+
+                // Construindo objeto InserirFuncionarioResponse
+                return new InserirFuncionarioResponse
+                {
+                    Funcionario = funcionario,
+                    StatusCode = System.Net.HttpStatusCode.OK,
+                    Mensagem = "Empresas obtidas com sucesso."
+                };
+            }
+            catch (Exception ex)
+            {
+                return new InserirFuncionarioResponse
+                {
+                    Funcionario = funcionario,
+                    StatusCode = System.Net.HttpStatusCode.InternalServerError,
+                    Mensagem = $"Ocorreu um erro ao inserir um funcionario: {ex.Message}"
+                };
+            }
+        }
+    }
+}
---
+++ Aula02_ControleDeFuncionarios/Aula02_ControleDeFuncionarios/Repositorios/FuncionarioRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using Aula02_ControleDeFuncionarios.Entidades;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace Aula02_ControleDeFuncionarios.Repositorios
+{
+    public class FuncionarioRepositorio
+    {
+
+        private readonly string _connectionString;
+
+        public FuncionarioRepositorio()
+        {
+            _connectionString = new Configuracoes.ConfiguracaoDB().ConnectioString;
+        }
+
+        public List<Funcionario> ObterFuncionarios()
+        {
+            var query = "SELECT * FROM funcionario order by nome";
+
+            var empresas = new List<Empresa>();
+
+            // Abrindo conexão com o banco de dados
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return connection.Query<Funcionario>(query).ToList();
+            }
+        }
+
+        public void InserirFuncionario(Funcionario funcionario)
+        {
+            var query = "INSERT INTO funcionario (id, nome, cpf, matricula, dataadmissao, empresaid) " +
+                        "VALUES (@Id, @Nome, @Cpf, @Matricula, @DataAdmissao, @EmpresaId)";
+
+            // Abrindo conexão com o banco de dados
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Execute(query, funcionario);
+            }
+
+        }
+    }
+}
