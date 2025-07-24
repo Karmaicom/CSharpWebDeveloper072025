5b95b015697696c229d8d1b59660bd6f4144ceb5
karmaicom <karmaicom@gmail.com>
2025-07-24 18:33:06 -0300

    Scripts

---
+++ Aula02_Scripts/SQLQuery1.sql
@@ -0,0 +1,27 @@
+﻿use DBFuncionarios;
+
+create table empresa (
+	id	uniqueidentifier not null primary key,
+	razaosocial	varchar(150) not null,
+	cnpj	varchar(20) not null unique
+);
+GO
+
+insert into empresa (id, razaosocial, cnpj) values
+	(newid(), 'Empresa A', '12.345.678/0001-90'),
+	(newid(), 'Empresa B', '98.765.432/0001-01'),
+	(newid(), 'Empresa C', '11.222.333/0001-44');
+GO
+
+create table funcionario (
+	id	uniqueidentifier not null primary key,
+	nome	varchar(100) not null,
+	cpf		varchar(14) not null unique,
+	matricula varchar(15) not null,
+	dataadmissao date not null,
+	empresaid	uniqueidentifier not null,
+	constraint fk_empresa foreign key (empresaid) references empresa(id)
+);
+GO
+
+Select * from empresa;
---
+++ Aula03_Scripts/Aula03/SQLQuery1.sql
@@ -0,0 +1,20 @@
+﻿use DBClients;
+
+create table client (
+	id			uniqueidentifier	not null primary key,
+	name		nvarchar(150)		not null,
+	email		nvarchar(50)		not null,
+	birthdate	date			not null	
+);
+Go
+
+
+insert into client(id, name, email, birthdate)
+values
+	(NEWID(), 'Fulano Silva', 'fulano@gmail.com', '2000-01-15'),
+	(NEWID(), 'Ciclano Moura', 'ciclano@gmail.com', '2005-10-04');
+Go
+
+select * from client;
+
+drop table client;	
