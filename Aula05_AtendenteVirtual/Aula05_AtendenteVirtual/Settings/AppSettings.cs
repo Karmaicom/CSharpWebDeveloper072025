4a879243159e0a5d46e00ff3ae48b8ccd3e74993
karmaicom <karmaicom@gmail.com>
2025-07-29 22:05:39 -0300

    feat: OpenAI

--- Aula05_AtendenteVirtual/Aula05_AtendenteVirtual/Settings/AppSettings.cs
+++ Aula05_AtendenteVirtual/Aula05_AtendenteVirtual/Settings/AppSettings.cs
@@ -18,7 +18,7 @@ public class AppSettings
         get
         {
             // Coloque aqui sua chave de API da OpenAI
-            return "key gerada no dashboard da openai"; 
+            return "code gerada no dashboard da openai"; 
         }
     }
 }
