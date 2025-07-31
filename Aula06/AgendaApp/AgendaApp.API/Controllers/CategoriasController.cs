cf9fbefa8bf7da9f4aad4ec3dac860af28150b05
karmaicom <karmaicom@gmail.com>
2025-07-31 20:18:27 -0300

    feat: Controllers iniciais

---
+++ Aula06/AgendaApp/AgendaApp.API/Controllers/CategoriasController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
