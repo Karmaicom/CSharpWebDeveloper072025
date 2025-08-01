fdc25aba440793a6be2d2bda4e7ffcf51b9cd819
karmaicom <karmaicom@gmail.com>
2025-07-31 21:11:19 -0300

    feat: Enums, Entidades e Pacotes

---
+++ Aula06/AgendaApp/AgendaApp.API/Entities/Categoria.cs
@@ -0,0 +1,12 @@
+﻿namespace AgendaApp.API.Entities
+{
+    public class Categoria
+    {
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Nome { get; set; } = string.Empty;
+
+        public List<Tarefa> Tarefas { get; set; } = new();
+
+    }
+}
---
+++ Aula06/AgendaApp/AgendaApp.API/Enums/Prioridade.cs
@@ -0,0 +1,9 @@
+﻿namespace AgendaApp.API.Enums
+{
+    public enum Prioridade
+    {
+        Alta = 1,
+        Média = 2,
+        Baixa = 3
+    }
+}
