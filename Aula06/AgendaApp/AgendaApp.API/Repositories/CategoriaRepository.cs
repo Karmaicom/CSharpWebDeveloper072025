237ac50b5879e2dd7d0bfd75bc8fd2d804e531e1
karmaicom <karmaicom@gmail.com>
2025-08-05 19:34:55 -0300

    feat: Repositorios de Tarefa e Categoria.

---
+++ Aula06/AgendaApp/AgendaApp.API/Repositories/CategoriaRepository.cs
@@ -0,0 +1,27 @@
+﻿using AgendaApp.API.Contexts;
+using AgendaApp.API.Entities;
+
+namespace AgendaApp.API.Repositories
+{
+    public class CategoriaRepository
+    {
+
+        /// <summary>
+        /// Método para consultar todas as categorias no banco de dados.
+        /// </summary>
+        public List<Categoria> ObterTodos()
+        {
+            using (var context = new DataContext())
+            {
+                /*
+                 * SELECT * FROM CATEGORIA 
+                 * ORDER BY NOME ASC
+                 */
+                return context
+                        .Set<Categoria>()
+                        .OrderBy(c => c.Nome)
+                        .ToList();
+            }
+        }
+    }
+}
---
+++ Aula06/AgendaApp/AgendaApp.API/Repositories/TarefaRepository.cs
@@ -0,0 +1,82 @@
+﻿using AgendaApp.API.Contexts;
+using AgendaApp.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaApp.API.Repositories
+{
+    public class TarefaRepository
+    {
+        /// <summary>
+        /// Método para inserir uma nova tarefa no banco de dados.
+        /// </summary>
+        /// <param name="tarefa"></param>
+        public void Inserir(Tarefa tarefa)
+        {
+            using (var context = new DataContext())
+            {
+                context.Add(tarefa);
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Método para atualizar uma tarefa no banco de dados.
+        /// </summary>
+        /// <param name="tarefa"></param>
+        public void Atualizar(Tarefa tarefa)
+        {
+            using(var context = new DataContext())
+            {
+                context.Update(tarefa);
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Método para excluir uma tarefa no banco de dados.
+        /// </summary>
+        /// <param name="tarefa"></param>
+        public void Excluir(Tarefa tarefa)
+        {
+            using (var context = new DataContext())
+            {
+                context.Remove(tarefa);
+                context.SaveChanges();
+            }
+        }
+
+
+        /// <summary>
+        /// Consulta tarefas por intervalo de datas.
+        /// </summary>
+        /// <param name="dataHotaInicio"></param>
+        /// <param name="dataHoraFim"></param>
+        /// <returns></returns>
+        public List<Tarefa> ObterPorDatas(DateTime dataHotaInicio, DateTime dataHoraFim)
+        {
+            using (var context = new DataContext())
+            {
+                return context
+                    .Set<Tarefa>()
+                    .Include(x => x.Categoria)
+                    .Where(x => x.DataHora >= dataHotaInicio && x.DataHora <= dataHoraFim)
+                    .OrderByDescending(x => x.DataHora)
+                    .ToList();
+            }
+        }
+
+        /// <summary>
+        /// Método para consultar 1 tarefa no banco de dados através do ID.
+        /// </summary>
+        public Tarefa? ObterPorId(Guid id)
+        {
+            using (var context = new DataContext())
+            {
+                return context
+                        .Set<Tarefa>()
+                        .Include(x => x.Categoria)
+                        .SingleOrDefault(t => t.Id == id);
+            }
+        }
+    }
+}
